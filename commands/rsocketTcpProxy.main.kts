0108cf971f4845613baf7b36fd287f376bcfb194
Yuri Schimke <yuri@schimke.ee>
2020-12-17 07:54:15 +0000

    Cleaner script

--- commands/rsocketTcpProxy.main.kts
+++ commands/rsocketTcpProxy.main.kts
@@ -1,27 +1,21 @@
 #!/usr/bin/env -S kotlinc-jvm -nowarn -script
 
-@file:Repository("https://jitpack.io")
-@file:Repository("https://dl.bintray.com/kotlin/kotlinx")
 @file:Repository("https://jcenter.bintray.com")
-@file:DependsOn("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.4.2-native-mt")
-@file:DependsOn("io.rsocket.kotlin:rsocket-core-jvm:0.12.0")
-@file:DependsOn("io.rsocket.kotlin:rsocket-transport-ktor-jvm:0.12.0")
 @file:DependsOn("io.rsocket.kotlin:rsocket-transport-ktor-client-jvm:0.12.0")
-@file:DependsOn("io.ktor:ktor-network-tls:1.4.3")
-@file:DependsOn("io.ktor:ktor-client-okhttp:1.4.3")
-@file:DependsOn("io.ktor:ktor-client-core-jvm:1.4.3")
 @file:DependsOn("io.ktor:ktor-client-cio-jvm:1.4.3")
-@file:DependsOn("io.ktor:ktor-server-cio:1.4.3")
 @file:CompilerOptions("-jvm-target", "1.8")
 
-import io.ktor.client.*
-import io.ktor.network.selector.*
-import io.ktor.network.sockets.*
-import io.rsocket.kotlin.core.*
-import io.rsocket.kotlin.logging.*
-import io.rsocket.kotlin.transport.ktor.*
-import io.rsocket.kotlin.transport.ktor.client.*
-import kotlinx.coroutines.*
+import io.ktor.client.HttpClient
+import io.ktor.network.selector.ActorSelectorManager
+import io.ktor.network.sockets.aSocket
+import io.rsocket.kotlin.core.RSocketServer
+import io.rsocket.kotlin.core.RSocketConnector
+import io.rsocket.kotlin.logging.LoggingLevel
+import io.rsocket.kotlin.logging.PrintLogger
+import io.rsocket.kotlin.transport.ktor.serverTransport
+import io.rsocket.kotlin.transport.ktor.client.rSocket
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import io.ktor.client.engine.cio.CIO as ClientCIO
 import io.ktor.client.features.websocket.WebSockets as ClientWebSockets
 import io.rsocket.kotlin.transport.ktor.client.RSocketSupport as ClientRSocketSupport
